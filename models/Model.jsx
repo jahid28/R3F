a84e443481d9e9c0dacd63c356e5597594891d45
jahid28 <jahidrhps123@gmail.com>
2024-05-08 19:57:49 +0530

    updates

--- models/Model.jsx
+++ models/Model.jsx
@@ -1,19 +1,40 @@
 import React, { useRef } from 'react'
-import { useGLTF } from '@react-three/drei'
-
+import { useGLTF,Text,MeshTransmissionMaterial } from '@react-three/drei'
+import { Canvas, useFrame,useThree } from "@react-three/fiber";
+import { useControls } from 'leva';
 export function Model(props) {
-  const { nodes, materials } = useGLTF('/Pyramid/scene.gltf')
+  const mesh=useRef()
+  useFrame(()=>{
+// mesh.current.rotation.x+=.004
+mesh.current.rotation.y+=.01
+  })
+
+  const materialProps=useControls({
+    thickness:{value:.2,min:0,max:30,step:.05},
+    roughness:{value:0,min:0,max:1,step:.1},
+    transmission:{value:1,min:0,max:1,step:.1},
+    ior:{value:1.2,min:0,max:3,step:.1},
+    chromaticAberration:{value:0.02,min:0,max:1},
+    // blur:{value:0.02,min:0,max:100,step:.5},
+    backside:{value:true}
+  })
+  
+  const { nodes, materials } = useGLTF('/book_pillar.glb')
   return (
-    <group {...props} dispose={null}>
-      <group position={[0.997, 0, 0]} rotation={[Math.PI / 2, 0, 0]}>
+    <group scale={2} ref={mesh} {...props} dispose={null}>
+      
+      <group  position={[0.997, 0, 0]} rotation={[Math.PI / 2, 0, 0]}>
         <mesh
+        
           castShadow
           receiveShadow
           geometry={nodes.Object_6.geometry}
           material={materials.paper}
           position={[-0.999, 0.002, -5.649]}
           scale={0.541}
-        />
+        >
+          
+        </mesh>
         <mesh
           castShadow
           receiveShadow
@@ -52,13 +73,16 @@ export function Model(props) {
       <group rotation={[-Math.PI / 2, 0, 0]} scale={4.902}>
         <group rotation={[Math.PI / 2, 0, 0]} scale={0.01}>
           <group position={[28.82, 0, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+           
             <mesh
               castShadow
               receiveShadow
               geometry={nodes.stone_pillar_01_stone_pillar_01_0.geometry}
               material={materials['Material.001']}
               position={[-28.931, 0.071, 57.945]}
-            />
+            >
+              <MeshTransmissionMaterial {...materialProps}/>
+            </mesh>
           </group>
         </group>
       </group>
@@ -66,4 +90,4 @@ export function Model(props) {
   )
 }
 
-useGLTF.preload('/Pyramid/scene.gltf')
+useGLTF.preload('/book_pillar.glb')
--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,18 @@ import { Suspense, useEffect, useRef, useState } from "react";
 import "./App.css";
 import * as THREE from "three";
 import gsap from "gsap";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
-import { Model } from "../models/Model.jsx";
-function App() {
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import {
+  Environment,
+  OrbitControls,
+  PerspectiveCamera,
+  OrthographicCamera,
+  Text,
+} from "@react-three/drei";
+import { ForestSpline } from "../models/Forest.jsx";
+import { Trees } from "../models/Trees.jsx";
+import { Mount } from "../models/Mount.jsx";
+function App(props) {
   const cameraRef = useRef();
   // const [isAnimating, setIsAnimating] = useState(false);
   const [boxColor, setBoxColor] = useState("yellow");
@@ -13,21 +21,82 @@ function App() {
   const animateCamera = () => {
     gsap.to(cameraRef.current.position, {
       duration: 2,
-      z: 8,
-      y: 3,
+      x:200,
+      z: 0,
+      y: 50,
       ease: "power1.inOut",
       // onComplete: () => setIsAnimating(false),
     });
   };
 
+  // animateCamera()
+
+  // window.addEventListener("scroll", function () {
+  //   if (this.scrollY > 200) {
+  //     animateCamera();
+  //   }
+  //   // const scrollPosition = window.scrollY;
+  //   // const fullHeight = document.documentElement.scrollHeight - window.innerHeight;
+  //   // if (scrollPosition >= fullHeight) {
+  //   //   animateCamera()
+  //   // } else {
+  //   //   // setShowPopup(false);
+  //   // }
+  //   console.log(this.scrollY);
+  // });
+  // const {viewport}=useThree()
+
   // useFrame(() => (box.current.rotation.x = box.current.rotation.y += 0.01));
 
   // const [helper] = useDirectionalLightHelper();
 
+  const env = useRef();
+  const dL = useRef();
+  const aL = useRef();
+
+  const [dataToSend, setDataToSend] = useState(1);
+  const [mode, setMode] = useState("city");
+
+  function func() {
+    if (dataToSend % 2 == 0) {
+      // setMode("night")
+      gsap.to(aL.current, {
+        duration: 2,
+        intensity: 2,
+        ease: "power1.inOut",
+      });
+      gsap.to(dL.current, {
+        duration: 2,
+        intensity: 4,
+        ease: "power1.inOut",
+      });
+    } else {
+      // setMode("city")
+      gsap.to(aL.current, {
+        duration: 2,
+        intensity: 0.1,
+        ease: "power1.inOut",
+      });
+      gsap.to(dL.current, {
+        duration: 2,
+        intensity: 0.5,
+        ease: "power1.inOut",
+      });
+    }
+    setDataToSend((e) => e + 1);
+  }
+
   return (
     <>
-      <h1>hello</h1>
-      <div id="canvas-container">
+      {/* <h1>hello</h1> */}
+      <div style={{ height: "200vh" }} id="canvas-container">
+        <button
+          onClick={func}
+          style={{ zIndex: 100, position: "absolute", top: 0 }}
+        >
+          Toggle
+        </button>
+
         {/* <Canvas
           camera={{
             fov: 75,
@@ -37,35 +106,58 @@ function App() {
           }}
           style={{ width: "50vw", height: "50vh" }}
         > */}
-        <button onClick={() => animateCamera()}>Animate Camera</button>
+        {/* <button onClick={() => animateCamera()}>Animate Camera</button> */}
 
-        <Canvas style={{ width: "100vw", height: "100vh" ,border:"2px solid red",marginLeft:"-17vh"}}>
+        <Canvas
+          style={{
+            position: "fixed",
+            top: "0",
+            left: "0vh",
+            width: "100vw",
+            height: "100vh",
+            // border: "2px solid red",
+            // backgroundColor: "#364059",
+          }}
+        >
           <PerspectiveCamera
             ref={cameraRef}
             far={40000}
             near={0.1}
             fov={70}
             makeDefault
-            position={[0, 10, 0]}
+            position={[100, 100, 100]}
           />
+          {/* <OrthographicCamera
+        ref={cameraRef}
+        left={-10}
+        right={10}
+        top={10}
+        bottom={-10}
+        near={0.1}
+        far={1000}
+        position={[-200, 230, -350]} // Optional: Set camera position
+        makeDefault // Optional: Makes this camera the default camera
+      /> */}
 
           <Suspense fallback={null}>
-            <ambientLight intensity={0.1} />
-            <directionalLight color="white" position={[0, 0, 5]} />
+            <ambientLight ref={aL} intensity={2} />
+            <directionalLight
+              ref={dL}
+              intensity={4}
+              color="white"
+              position={[0, 4, 2]}
+            />
             {/* <directionalLightHelper target={directionalLightRef.current} size={10} /> */}
-
-            {/* <Model /> */}
-
-            <mesh
-              onClick={() => {
-                setBoxColor("red");
+            {/* <Environment  preset={"night"} environmentIntensity={1}/> */}
+            {/* <Text fontSize={5} position={[0,9,-4]}>
+  hello
+</Text> */}
+            <Trees />
+            <Mount
+              onSubmit={() => {
+                animateCamera();
               }}
-              ref={box}
-              position={[0, 0, 0]}
-            >
-              <boxGeometry args={[2, 2, 2]} />
-              <meshStandardMaterial color={boxColor} />
-            </mesh>
+            />
             <OrbitControls />
             <axesHelper args={[20]} />
           </Suspense>
